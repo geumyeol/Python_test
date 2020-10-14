670d5432152dca09fadadbe3827138f038e121a7
geumyeol <1004gmyoul@naver.com>
2020-10-14 22:00:18 +0900

    start_projcet_success

--- kakao_test/stockList.py
+++ kakao_test/stockList.py
@@ -54,7 +54,7 @@ def getDataOfParam(index, stock_name, param):
 
         if index == 0 and idx==7 :
             if float(temp_data) > float(ROE_BASE):
-                s = "## [{}] - ROE_BASE:{} 달성 - {}".format(stock_name, ROE_BASE, float(temp_data))
+                s = "# ROE_BASE:{} 달성 => {}".format( ROE_BASE, float(temp_data))
                 stock_message.append(s)
                 # print(s)
             else:
@@ -65,7 +65,7 @@ def getDataOfParam(index, stock_name, param):
 
         elif index == 1 and idx==7 :
             if float(temp_data) < float(PER_BASE):
-                s = "## [{}] - PER_BASE:{} 달성 - {}".format(stock_name, PER_BASE, float(temp_data))
+                s = "# PER_BASE:{} 달성 => {}".format( PER_BASE, float(temp_data))
                 stock_message.append(s)
                 # print(s)
             else:
@@ -76,7 +76,7 @@ def getDataOfParam(index, stock_name, param):
 
         elif index == 2 and idx==7 :
             if float(temp_data) < PBR_BASE:
-                s = "## [{}] - PBR_BASE:{} 달성 - {}".format(stock_name, PBR_BASE, float(temp_data))
+                s = "# PBR_BASE: {} 달성 => {}".format( PBR_BASE, float(temp_data))
                 stock_message.append(s)
                 # print(s)
             else:
@@ -95,101 +95,153 @@ def getDataOfParam(index, stock_name, param):
 # f = open(filename, "w", encoding="utf-8-sig", newline='')
 # writer = csv.writer(f, delimiter='\t')
 
-url = "https://finance.naver.com/sise/sise_market_sum.nhn"
 
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'lxml')
 
-stock_head = soup.find("thead").find_all("th")
-data_head = [head.get_text() for head in stock_head]
-data_head.pop()
-
-stock_list = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("tr")
-stockTop50_corp = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("a", attrs={"class": "tltle"})
-result_str = ""
-
-data_header = ['회사명']
 idx = 1
-
 Total_Message = []
 
+for page_num in range(1,5):
 
-for stock in stockTop50_corp:
-    corp_Message = []
-    stock_name = stock.get_text()
-    stock_link = "https://finance.naver.com/"+stock["href"]
-    flag = 1
+    url = "https://finance.naver.com/sise/sise_market_sum.nhn?page="+"%d"%(page_num)
 
-    sub_res = requests.get(stock_link)
-    sub_soup = BeautifulSoup(sub_res.text, 'lxml')
+    # print(url)
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
 
-    sub_thead = sub_soup.find("table", attrs={"class":"tb_type1 tb_num tb_type1_ifrs"}).\
-        find("thead").find_all("th", attrs={"scope":"col"})
+    stock_head = soup.find("thead").find_all("th")
+    data_head = [head.get_text() for head in stock_head]
+    data_head.pop()
 
-    img_link_list = ['month3', 'year', 'year3']
-    img_link = sub_soup.find("img", attrs={"id": "img_chart_area"})['src']
+    stock_list = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("tr")
+    stockTop50_corp = soup.find("table", attrs={"class": "type_2"}).find("tbody").find_all("a", attrs={"class": "tltle"})
+    result_str = ""
 
-    # print(stock_name)
+    data_header = ['회사명']
 
-    if idx==1:
-        result_str = sub_soup.find("th", attrs={"class": "h_th2 th_cop_anal5 b_line"}).get_text()
-        data_header.append(result_str)
+    for stock in stockTop50_corp:
+        corp_Message = []
+        stock_name = stock.get_text()
+        stock_link = "https://finance.naver.com/"+stock["href"]
+        # print(stock_link)
 
-        for value in sub_thead:
-            str = value.get_text().strip()
-            if (str.startswith("20")):
-                result_str += str
-                data_header.append(str)
+        if str(stock_name).find("KODEX") >= 0 or \
+                str(stock_name).find("맥쿼리인프라") >= 0 or \
+                str(stock_name).find("TIGER") >= 0 or \
+                str(stock_name).find("롯데리츠") >= 0 or \
+                str(stock_name).find("티와이홀딩스") >= 0 or \
+                str(stock_name).find("200") >= 0:
 
-        idx += 1
-        # print(data_header)
+            continue
 
-    # ParamList = ['매출액', '영업이익', '당기순이익', 'ROE(지배주주)', 'PER(배)', 'PBR(배)']
-    ParamList = ['ROE(지배주주)', 'PER(배)', 'PBR(배)']
+        flag = 1
 
-    for idx, pText in enumerate(ParamList):
+        sub_res = requests.get(stock_link)
+        sub_soup = BeautifulSoup(sub_res.text, 'lxml')
 
-        param = " ".join(sub_soup.find('strong', text=pText).parent['class'])
-        # print (param)
-        result_message = getDataOfParam(idx, stock_name, param)
-        corp_Message.extend(result_message)
-        if len(result_message) != 0 :
-            temp = result_message.pop()
-            if temp.startswith("@@"):
-                flag = 0
-                # img1 = img_link.replace("day", img_link_list[0])
-                # img2 = img_link.replace("day", img_link_list[1])
-                # img3 = img_link.replace("day", img_link_list[2])
-                # Total_Message.append(temp)
-                # Total_Message.append(img1)
-                # Total_Message.append(img2)
-                # Total_Message.append(img3)
+        sub_thead = sub_soup.find("table", attrs={"class":"tb_type1 tb_num tb_type1_ifrs"}).\
+            find("thead").find_all("th", attrs={"scope":"col"})
 
-                break
+        img_link_list = ['month3', 'year', 'year3']
+        img_link = sub_soup.find("img", attrs={"id": "img_chart_area"})['src']
 
-        # Total_Message.extend(result_message)
-        # print (len(result_message))
-        # print(result_message)
+        # print(stock_name)
 
-    # img_link_list = ['month3', 'year', 'year3']
-    # img_link = sub_soup.find("img", attrs={"id": "img_chart_area"})['src']
+        if idx==1:
+            result_str = sub_soup.find("th", attrs={"class": "h_th2 th_cop_anal5 b_line"}).get_text()
+            data_header.append(result_str)
 
-    if flag == 0:
+            for value in sub_thead:
+                str__ = value.get_text().strip()
+                if (str__.startswith("20")):
+                    result_str += str__
+                    data_header.append(str__)
 
-        continue
+            idx += 1
+            # print(data_header)
 
-    Total_Message.extend(corp_Message)
-    img1 = img_link.replace("day", img_link_list[0])
-    img2 = img_link.replace("day", img_link_list[1])
-    img3 = img_link.replace("day", img_link_list[2])
-    Total_Message.append(img1)
-    Total_Message.append(img2)
-    Total_Message.append(img3)
+        # ParamList = ['매출액', '영업이익', '당기순이익', 'ROE(지배주주)', 'PER(배)', 'PBR(배)']
+        ParamList = ['ROE(지배주주)', 'PER(배)', 'PBR(배)']
 
-    # writer.writerow(stock_name)
-    # writer.writerow(result_str)
-    # print(stock_name)
-    # print(result_str)
+        for idx, pText in enumerate(ParamList):
+
+            param = " ".join(sub_soup.find('strong', text=pText).parent['class'])
+            # print (param)
+            result_message = getDataOfParam(idx, stock_name, param)
+            corp_Message.extend(result_message)
+            if len(result_message) != 0 :
+                temp = result_message.pop()
+                if temp.startswith("@@"):
+                    flag = 0
+                    # img1 = img_link.replace("day", img_link_list[0])
+                    # img2 = img_link.replace("day", img_link_list[1])
+                    # img3 = img_link.replace("day", img_link_list[2])
+                    # Total_Message.append(temp)
+                    # Total_Message.append(img1)
+                    # Total_Message.append(img2)
+                    # Total_Message.append(img3)
+
+                    break
+
+            # Total_Message.extend(result_message)
+            # print (len(result_message))
+            # print(result_message)
+
+        # img_link_list = ['month3', 'year', 'year3']
+        # img_link = sub_soup.find("img", attrs={"id": "img_chart_area"})['src']
+
+        if flag == 0:
+
+            continue
+
+        if corp_Message:
+            Total_Message.append("<a href='"+stock_link+"'>"+ stock_name+"</a>")
+            Total_Message.extend(corp_Message)
+            img1 = img_link.replace("day", img_link_list[0])
+            img2 = img_link.replace("day", img_link_list[1])
+            img3 = img_link.replace("day", img_link_list[2])
+            Total_Message.append("<br>[3개월]<br><img src='"+img1+"'>")
+            Total_Message.append("[1년]<br><img src='"+img2+"'>")
+            Total_Message.append("[3년]<br><img src='"+img3+"'>")
+            Total_Message.append("")
+
+        # writer.writerow(stock_name)
+        # writer.writerow(result_str)
+        # print(stock_name)
+        # print(result_str)
 
 
 print("\n".join(Total_Message))
+
+# E-mail 전송
+
+import smtplib, os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+
+id = '1004gmyoul@naver.com'
+password = 'qhdksdls!2'
+sendEmail = '1004gmyoul@naver.com'
+today = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+subject = '['+today+' 주식추천 종목]'
+text = "<br>".join(Total_Message)
+addrs = ['1004gmyoul@naver.com', 'godshy1611@naver.com']  # send mail list
+
+# login
+smtp = smtplib.SMTP('smtp.naver.com', 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(id, password)
+
+for addr in addrs:
+    # message
+    message = MIMEMultipart()
+    message.attach(MIMEText(text, 'html'))
+
+    # Send
+    message["From"] = sendEmail
+    message["To"] = addr
+    message['Subject'] = subject
+    smtp.sendmail(sendEmail, addr, message.as_string())
+
+smtp.quit()
